827660549408d89c2c74de3121fdb1bc01a27d84
Stany Dsouza <220361581+stany-clearquote@users.noreply.github.com>
2026-08-10 21:09:35 +0530

    v0.1.6-alpha.2

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ClearQuoteSDK",
-            url: "https://github.com/clearquotetech/cq-ios-sdk/releases/download/v0.1.6-alpha.1/ClearQuoteSDK.xcframework.zip",
-            checksum: "a7956f164d32bc4dabf20097df528630c75803d24d0e2a26da6995cceb6b7f39"
+            url: "https://github.com/clearquotetech/cq-ios-sdk/releases/download/v0.1.6-alpha.2/ClearQuoteSDK.xcframework.zip",
+            checksum: "3589824d85a6bbe88de5ca0c4d731b88b44b0be20af248d548ff5a6b074053a3"
         ),
         .target(
             name: "ClearQuoteSDKDeps",
